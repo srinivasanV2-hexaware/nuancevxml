dee1ceac7303503db3790a6af357e466d5dc21ad
srini23121987 <ursfriendly.sri23@gmail.com>
2017-12-03 20:25:14 +0530

    new commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/public")));
+app.get("/", function (req, res) {
+    console.log("GET");
+    res.sendFile(__dirname+'/public/welcome.vxml');
+})
+app.post("/", function (req, res) {
+    console.log("POST");
+    res.contentType('application/xml');
+    res.sendFile(__dirname+'/public/welcome.vxml');
+})
+app.listen(3000);
